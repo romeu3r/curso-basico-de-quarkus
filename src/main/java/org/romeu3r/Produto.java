22eeb9eabd07bfefd1c0209ec85067548cc8db47
Willian Nogueira <willian.silva@sciensa.com>
2022-06-03 13:02:40 -0300

    Testes ok, adicionando dependencias: StAX API, rider-core & rider-cdi

--- src/main/java/org/romeu3r/Produto.java
+++ src/main/java/org/romeu3r/Produto.java
@@ -4,10 +4,12 @@ import io.quarkus.hibernate.orm.panache.PanacheEntity;
 import org.hibernate.annotations.CreationTimestamp;
 
 import javax.persistence.Entity;
+import javax.persistence.Table;
 import java.math.BigDecimal;
 import java.util.Date;
 
 @Entity
+@Table(name = "PRODUTO")
 public class Produto extends PanacheEntity {
     public String nome;
     public BigDecimal preco;
--- src/test/java/org/romeu3r/ProdutoTeste.java
+++ src/test/java/org/romeu3r/ProdutoTeste.java
@@ -1,15 +1,19 @@
 package org.romeu3r;
 
+import com.github.database.rider.cdi.api.DBRider;
+import com.github.database.rider.core.api.dataset.DataSet;
 import io.quarkus.test.common.QuarkusTestResource;
 import io.quarkus.test.junit.QuarkusTest;
 import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 
+@DBRider
 @QuarkusTest
 @QuarkusTestResource(DataBaseLifeCycle.class)
 public class ProdutoTeste {
 
     @Test
+    @DataSet("org/romeu3r/produtos1.yml")
     public void testeUm() {
         Assert.assertEquals(1, Produto.count());
     }
